110f430a35f12e20550fd16ff3f587c19498457c
aibrohim <ibrohim7840@gmail.com>
2021-08-12 09:02:57 +0500

    added users page

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -34,11 +34,55 @@ const Home = () => {
               <input className="new-event__input" type="text" placeholder="some text"/>
             </label>
           </div>
-          <div>
-            <button type="reset">Bekor qilish</button>
+          <div className="new-event__actions">
+            <button className="new-event__cancel submit-btn submit-btn--outlined" type="reset">Bekor qilish</button>
             <button className="submit-btn new-event__submit" type="submit">Bekor qilish</button>
           </div>
         </form>
+      </section>
+      <section className="section">
+        <h2 className="section__title">Barcha tadbirlar</h2>
+        <ul className="events">
+          <li className="events__item">
+            <div className="events__item-content">
+              <h3 className="events__item-title">🎯Tajribali va muvaffaqiyatli soha liderlari nutqini o'tkazib yubormaslik uchun hoziroq ro'yxatdan o'ting.</h3>
+              <p className="events__item-desc">Yurtimizda belgilangan karantin cheklovlari sabab, loyihamizni yirik zallarda o'tkazish imkoni hozircha mavjud emas. Ammo bu sizga yangi bilimlarni yetkazishimizga to'sqinlik qila olmaydi...</p>
+            </div>
+            <div className="events__item-info">
+              <time className="events__item-info-item events__item-date">11.08.2021</time>
+              <p className="events__item-info-item events__item-users">400</p>
+              <button className="events__item-delete">
+                <span>O'chirish</span>
+              </button>
+            </div>
+          </li>
+          <li className="events__item">
+            <div className="events__item-content">
+              <h3 className="events__item-title">🎯Tajribali va muvaffaqiyatli soha liderlari nutqini o'tkazib yubormaslik uchun hoziroq ro'yxatdan o'ting.</h3>
+              <p className="events__item-desc">Yurtimizda belgilangan karantin cheklovlari sabab, loyihamizni yirik zallarda o'tkazish imkoni hozircha mavjud emas. Ammo bu sizga yangi bilimlarni yetkazishimizga to'sqinlik qila olmaydi...</p>
+            </div>
+            <div className="events__item-info">
+              <time className="events__item-info-item events__item-date">11.08.2021</time>
+              <p className="events__item-info-item events__item-users">400</p>
+              <button className="events__item-delete">
+                <span>O'chirish</span>
+              </button>
+            </div>
+          </li>
+          <li className="events__item">
+            <div className="events__item-content">
+              <h3 className="events__item-title">🎯Tajribali va muvaffaqiyatli soha liderlari nutqini o'tkazib yubormaslik uchun hoziroq ro'yxatdan o'ting.</h3>
+              <p className="events__item-desc">Yurtimizda belgilangan karantin cheklovlari sabab, loyihamizni yirik zallarda o'tkazish imkoni hozircha mavjud emas. Ammo bu sizga yangi bilimlarni yetkazishimizga to'sqinlik qila olmaydi...</p>
+            </div>
+            <div className="events__item-info">
+              <time className="events__item-info-item events__item-date">11.08.2021</time>
+              <p className="events__item-info-item events__item-users">400</p>
+              <button className="events__item-delete">
+                <span>O'chirish</span>
+              </button>
+            </div>
+          </li>
+        </ul>
       </section>
     </div>
   );
--- src/screens/Users/Users.js
+++ src/screens/Users/Users.js
@@ -1,6 +1,51 @@
 const Users = () => {
   return (
-    <h1>Users</h1>
+    <div className="container">
+      <form className="choose-winner">
+        <div className="choose-winner__block choose-winner__event">
+          <label className="choose-winner__title">Tadbir</label>
+          <select className="field choose-winner__select" name="" id="">
+            <option value="1">Salom</option>
+            <option value="2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium ipsum, mollitia et quam esse voluptatibus deserunt quos eum aperiam! Praesentium.</option>
+          </select>
+        </div>
+        <div className="choose-winner__block choose-winner__winner">
+          <h2 className="choose-winner__title">G’oliblarni aniqlash</h2>
+          <div className="choose-winner__right-content">
+            <input className="choose-winner__input field" type="number" />
+            <button className="submit-btn" type="submit">Randomlash</button>
+          </div>
+        </div>
+      </form>
+
+      <div className="users">
+        <table className="users__table">
+          <caption className="users__title">Ro’yxatdan o’tganlar (378)</caption>
+          <thead>
+            <tr>
+              <th>Ism va familiya</th>
+              <th>Telefon raqam</th>
+              <th>Yosh</th>
+              <th>Joylashuv</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                Muxammadjon Poziljonov
+                <br />
+                #1547896570
+              </td>
+              <td>
+                <a href="tel:+998903489866">+998 90 348 98 66</a>
+              </td>
+              <td>86</td>
+              <td>Farg'ona</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
   );
 };
 
